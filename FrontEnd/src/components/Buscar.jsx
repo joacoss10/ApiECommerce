60b78296e55cc0c2adca4498db33471ff7690266
Bautista Salvalai <bautistasalvalai02@gmail.com>
2024-07-01 20:52:28 -0300

    Hola

--- FrontEnd/src/components/Buscar.jsx
+++ FrontEnd/src/components/Buscar.jsx
@@ -3,35 +3,53 @@ import '../styles/Buscar.css'
 import productos from '../utils/productos.json'
 import { useState, useEffect } from 'react'
 import Card from "./Card.jsx";
+import { Button } from '@mui/material';
 
 
 
 function Buscar() {
-  const [buscar, setBuscar] = useState('');
+  const [botonPalabra, setBotonPalabra] = useState('');
+  const [palabra, setPalabra] = useState('');
   const [listaProductos, setListaProductos] = useState([]);
 
-  const handleBuscar = (e) => {
-    setBuscar(e.target.value);
+  const handlePalabra = (e) => {
+    setPalabra(e.target.value);
   };
 
-  useEffect(() => {
-    if (buscar) {
-      const nuevosProductos = productos.filter(p => {
-        return p.nombre.toLowerCase().includes(buscar.toLowerCase());
-      });
-      setListaProductos(nuevosProductos);
-    }
-    else {
-      setListaProductos([]);
-    }
-  }, [buscar]
-  )
+  const handleBoton = () => {
+    setBotonPalabra(palabra);
+  };
+
+  useEffect(() =>{
+
+    const fetchBusqueda = async () => {
+      try {
+        const response = await fetch(`http://localhost:8080/product/search?nombre=${botonPalabra}`,{
+          method: 'GET',
+          headers: {
+          'Content-Type': 'application/json',
+          'Access-Control-Allow-Origin': '*' 
+          }
+        });
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        const data = await response.json();
+        console.log(data);
+        setListaProductos(data);
+      } catch (error) {
+        console.log("Hubo un error");
+      }
+    };
+    fetchBusqueda();
+  },[botonPalabra]);
 
   return (
-
     <div className='Contenedor'>
-      <input id='input' type="text" onChange={handleBuscar} />
-
+      <div className='barra-busqueda'>
+        <input id='input' type='text' value={palabra} onChange={handlePalabra} />
+        <Button id='boton' onClick={handleBoton}>Buscar</Button>
+      </div>
       <div className='buscar-productos'>
         <div className='buscar-cards'>
           {listaProductos.length > 0 ? (
@@ -44,7 +62,7 @@ function Buscar() {
         </div>
       </div>
     </div>
-  )
+  );
 }
-
 export default Buscar
+
